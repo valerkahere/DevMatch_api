5b492b07bb9fe974218ea9d97fdfe002a57a2408
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-26 22:29:23 +0100

    Match class-validator 0.14 to nestjs 11 - more stable

---
+++ src/profiles/dto/update-profile.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+export class UpdateProfileDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  description: string;
+}
--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Put } from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
-
+import { UpdateProfileDto } from './dto/update-profile.dto';
 @Controller('profiles')
 export class ProfilesController {
-  // @Get
-  // getAll() {
-  //   return
-  // }
-
+  // GET /profiles
   @Get()
-  getLocation(@Query('location') location: string) {
-    return [{ location }];
+  findAll() {
+    return [];
   }
 
   // GET /profiles/:id
-  /* Challenge:
-    1. Set up the route for returning a single profile
-    2. It should take an ID param and return an object with that ID
-  */
   @Get(':id')
-  getOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string) {
     return { id };
   }
 
   // POST /profiles
+  // bash post.sh
   @Post()
-  create(@Body() profile: CreateProfileDto) {
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return createProfileDto;
+  }
+
+  // PUT /profiles/:id
+  /* 
+  Challenge:
+    1. Create a class named UpdateProfileDto in update-profile.dto.ts with both name and description as strings, and export that class.
+    2. Create a route in profiles.controller.ts to handle a PUT request. It should take in an ID as a param, and a body with a name and description. Then, return an object with the id, name, and description as a response.
+  
+  Note: Don't forget that you can type 'bash put.sh' in the terminal and hit enter to test out your new route.
+  */
+  @Put(':id')
+  update(@Param('id') id: string, @Body() profile: UpdateProfileDto) {
     return {
+      // Provide explicit keys (Recommended)
+      // Assign a clear key name to each property so the runtime knows how to build the object
       name: profile.name,
       description: profile.description,
+      id: id,
     };
   }
+
+  // DELETE /profiles/:id
 }
